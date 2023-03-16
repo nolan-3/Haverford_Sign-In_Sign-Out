6274fd21fea5e10cf60970699ae36db4a91525ed
Nolan McCloskey <nolamccl@haverford.org>
2023-03-16 19:35:13 -0400

    testing tasks

--- script.py
+++ script.py
@@ -1,11 +1,15 @@
 from datetime import datetime
 import json
 from send import send
+import time
 
-filename = datetime.now().strftime("%Y-%m-%d") + ".json"
+# filename = datetime.now().strftime("%Y-%m-%d") + ".json"
 
-with open(filename, "r") as file:
-    data = json.loads(file.read())
-    send(data)
+# with open(filename, "r") as file:
+#     data = json.loads(file.read())
+#     send(data)
+print("INSTANT!")
+time.sleep(100)
+print("AFTER 100 SECONDS!")
 
 
