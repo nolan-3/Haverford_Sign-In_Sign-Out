ae69f6718e673664bdad3ac0e57c07d39266a14d
Nolan McCloskey <nolamccl@haverford.org>
2023-08-18 23:26:55 -0400

    whoops not going to send any more emails to actual students over the summer haha :)

--- script.py
+++ script.py
@@ -13,15 +13,39 @@ if day_of_week == "Wednesday":
     
 
 filename = datetime.now().strftime("%Y-%m-%d.json")
-with open(filename, "r") as file:
-    data = json.loads(file.read())
-    send_not_signed_in_students(data)
-    print("Sign In Email Sent")
+# with open(filename, "r") as file:
+#     data = json.loads(file.read())
+#     send_not_signed_in_students(data)
+print("Sign In Email Sent")
 
 if day_of_week == "Wednesday":
-    time.sleep(18000)
+    time.sleep(3000)
+    print("slept for 3000 seconds")
+    time.sleep(3000)
+    print("slept for 6000 seconds")
+    time.sleep(3000)
+    print("slept for 9000 seconds")
+    time.sleep(3000)
+    print("slept for 12000 seconds")
+    time.sleep(3000)
+    print("slept for 15000 seconds")
+    time.sleep(3000)
+    print("slept for 18000 seconds")
+
 else:
-    time.sleep(19800)
+    time.sleep(3300)
+    print("slept for 3300 seconds")
+    time.sleep(3300)
+    print("slept for 6600 seconds")
+    time.sleep(3300)
+    print("slept for 9900 seconds")
+    time.sleep(3300)
+    print("slept for 13200 seconds")
+    time.sleep(3300)
+    print("slept for 16500 seconds")
+    time.sleep(3300)
+    print("slept for 19800 seconds")
+
 print("task slept until 3:15")
 
 filename = datetime.now().strftime("%Y-%m-%d.json")
