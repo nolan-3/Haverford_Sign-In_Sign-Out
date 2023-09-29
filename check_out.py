372e5838d27f0e210651f3575152cba2cd93efb7
Nolan McCloskey <nolamccl@haverford.org>
2023-09-29 14:35:17 -0400

    handled edge case where program is not reloaded for a full day

--- check_out.py
+++ check_out.py
@@ -6,26 +6,33 @@ from datetime import datetime
 from custom_logging import _read_or_initialize_student_file
 from custom_logging import _filename
 from custom_logging import _write_student_file
+from school_schedule import sign_out_open, registration_open
+from sign_in import sign_in_student
 
 def not_checked_out_names(grades=["V", "VI"]):
     date = datetime.now()
     students = _read_or_initialize_student_file(date)
     return [name for name, info in students.items() if info["grade"] in grades and not info["checked_out"]]
 
-
 def check_out_student(name):
     date = datetime.now()
     students = _read_or_initialize_student_file(date)
-    if name not in students:
-        print(f"WARNING: Student '{name}' not found in {_filename(date)}.")
+    # edge case where the program has not been reloaded since 3:15 the previous day
+    if registration_open():
+        sign_in_student(name)
+    else:
+        if name not in students:
+            print(f"WARNING: Student '{name}' not found in {_filename(date)}.")
+        if students[name]["checked_out"]:
+            print(f"WARNING Student {name} is already checked out in {_filename(date)}.")
+        if not students[name]["checked_out"]:
+            print(f"Checked out: {name}")
+            
+        students[name]["checked_out"] = True
+        _write_student_file(students, date)
+    
 
-    if students[name]["checked_out"]:
-        print(f"WARNING Student {name} is already checked out in {_filename(date)}.")
-    if not students[name]["checked_out"]:
-        print(f"Checked out: {name}")
-        
-    students[name]["checked_out"] = True
-    _write_student_file(students, date)
+
 
 
 
